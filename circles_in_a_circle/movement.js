ae3120975655d7f1dd1c5e51d9f22eede4ff8dff
Fania Everitt <me@fania.eu>
2025-05-13 19:45:54 +0200

    fix responsive design

---
+++ circles_in_a_circle/movement.js
@@ -0,0 +1,10 @@
+console.log('test');
+
+const circlesParent = document.querySelector('.circles');
+const circles = [...circlesParent.children];
+console.log(circles);
+
+const linesParent = document.querySelector('.lines');
+const lines = [...linesParent.children];
+console.log(lines);
+
